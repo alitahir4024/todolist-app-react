adca7774112919e965c37f0108843227cbd7c6af
Ali-Tahir4024 <alitahir4024@gmail.com>
2020-09-22 14:26:08 +0500

    Updates

---
+++ precache-manifest.000d8c54654fe9038c881884f5631c37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e2360acab8b75f350c13f6ae279e84",
+    "url": "/todolist-app-react/index.html"
+  },
+  {
+    "revision": "1d7713242de2b29bca86",
+    "url": "/todolist-app-react/static/css/2.820354c3.chunk.css"
+  },
+  {
+    "revision": "e2f0e48477b176e3a8a2",
+    "url": "/todolist-app-react/static/css/main.3168e48f.chunk.css"
+  },
+  {
+    "revision": "1d7713242de2b29bca86",
+    "url": "/todolist-app-react/static/js/2.674e9865.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todolist-app-react/static/js/2.674e9865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f0e48477b176e3a8a2",
+    "url": "/todolist-app-react/static/js/main.ad90a958.chunk.js"
+  },
+  {
+    "revision": "f523cf377c70ce6a0026",
+    "url": "/todolist-app-react/static/js/runtime-main.8bbe9c90.js"
+  }
+]);
